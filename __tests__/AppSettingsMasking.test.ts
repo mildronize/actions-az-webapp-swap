21d9a5da5f00b389af2e46021060728595a5e351
Thada Wangthammang <thada.wth@gmail.com>
2022-11-30 22:59:03 +0700

    fix: connection string should always sensitive value

--- __tests__/AppSettingsMasking.test.ts
+++ __tests__/AppSettingsMasking.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@jest/globals';
+import { expect, test, describe } from '@jest/globals';
 import { AppSettingsType } from '../src/core/AppSettingsBase';
 import AppSettingsMasking from '../src/core/AppSettingsMasking';
 import { DefaultSensitiveEnum, DefaultSlotSettingEnum, IAppSetting, ISwapAppService } from '../src/interfaces';
@@ -8,138 +8,177 @@ const globalConfig = {
   resourceGroup: 'resourceGroup name',
   slot: 'production',
   targetSlot: 'staging',
-  defaultSensitive: DefaultSensitiveEnum.required,
-  defaultSlotSetting: DefaultSlotSettingEnum.required,
+  defaultSlotSetting: DefaultSlotSettingEnum.required, // This will not effect in test
 };
 
-test('AppSettingsMasking.mask() (AppSettings) should return hash value in appSettings if sensitive is true', () => {
-  const swapAppService: ISwapAppService = {
-    ...globalConfig,
-    connectionStrings: [],
-    appSettings: [
+describe('App Setting Type', () => {
+  test('AppSettingsMasking.mask() (AppSettings) should return hash value in appSettings if sensitive is true', () => {
+    const swapAppService: ISwapAppService = {
+      ...globalConfig,
+      defaultSensitive: DefaultSensitiveEnum.required,
+      connectionStrings: [],
+      appSettings: [
+        {
+          name: 'data',
+          sensitive: true,
+          slotSetting: true,
+        },
+      ],
+    };
+
+    const appSettings: IAppSetting[] = [
       {
         name: 'data',
-        sensitive: true,
+        value: 'value',
         slotSetting: true,
       },
-    ],
-  };
+    ];
 
-  const appSettings: IAppSetting[] = [
-    {
-      name: 'data',
-      value: 'value',
-      slotSetting: true,
-    },
-  ];
+    const expected: IAppSetting[] = [
+      {
+        name: 'data',
+        value: 'ks3l7pl3EGX1QjFFGkZTcQcdizooEKftwwd8/sZ+o4aasgytauZM4N0hz30fTVkpI821ZoMzii3yMw9H6Sz9PQ==',
+        slotSetting: true,
+      },
+    ];
 
-  const expected: IAppSetting[] = [
-    {
-      name: 'data',
-      value: 'ks3l7pl3EGX1QjFFGkZTcQcdizooEKftwwd8/sZ+o4aasgytauZM4N0hz30fTVkpI821ZoMzii3yMw9H6Sz9PQ==',
-      slotSetting: true,
-    },
-  ];
+    const appSettingsMasking = new AppSettingsMasking(swapAppService, AppSettingsType.AppSettings);
+    expect(appSettingsMasking.mask(appSettings, 'staging')).toStrictEqual(expected);
+  });
 
-  const appSettingsMasking = new AppSettingsMasking(swapAppService, AppSettingsType.AppSettings);
-  expect(appSettingsMasking.mask(appSettings, 'staging')).toStrictEqual(expected);
+  test('AppSettingsMasking.mask() (AppSettings) should return actual value in appSettings if sensitive is false', () => {
+    const swapAppService: ISwapAppService = {
+      ...globalConfig,
+      defaultSensitive: DefaultSensitiveEnum.required,
+
+      connectionStrings: [],
+      appSettings: [
+        {
+          name: 'data',
+          sensitive: false,
+          slotSetting: true,
+        },
+      ],
+    };
+
+    const appSettings: IAppSetting[] = [
+      {
+        name: 'data',
+        value: 'value',
+        slotSetting: true,
+      },
+    ];
+
+    const expected: IAppSetting[] = [
+      {
+        name: 'data',
+        value: 'value',
+        slotSetting: true,
+      },
+    ];
+
+    const appSettingsMasking = new AppSettingsMasking(swapAppService, AppSettingsType.AppSettings);
+    expect(appSettingsMasking.mask(appSettings, 'staging')).toStrictEqual(expected);
+  });
 });
 
-test('AppSettingsMasking.mask() (AppSettings) should return actual value in appSettings if sensitive is false', () => {
-  const swapAppService: ISwapAppService = {
-    ...globalConfig,
-    connectionStrings: [],
-    appSettings: [
+describe('Connection String Type', () => {
+  test('AppSettingsMasking.mask() (connectionStrings) should return always hash value in connectionStrings if sensitive is true', () => {
+    const swapAppService: ISwapAppService = {
+      ...globalConfig,
+      defaultSensitive: DefaultSensitiveEnum.required,
+
+      connectionStrings: [
+        {
+          name: 'data',
+          sensitive: true,
+          slotSetting: true,
+        },
+      ],
+      appSettings: [],
+    };
+
+    const appSettings: IAppSetting[] = [
       {
         name: 'data',
-        sensitive: false,
+        value: 'value',
         slotSetting: true,
       },
-    ],
-  };
+    ];
 
-  const appSettings: IAppSetting[] = [
-    {
-      name: 'data',
-      value: 'value',
-      slotSetting: true,
-    },
-  ];
-
-  const expected: IAppSetting[] = [
-    {
-      name: 'data',
-      value: 'value',
-      slotSetting: true,
-    },
-  ];
-
-  const appSettingsMasking = new AppSettingsMasking(swapAppService, AppSettingsType.AppSettings);
-  expect(appSettingsMasking.mask(appSettings, 'staging')).toStrictEqual(expected);
-});
-
-test('AppSettingsMasking.mask() (connectionStrings) should return always hash value in connectionStrings if sensitive is true', () => {
-  const swapAppService: ISwapAppService = {
-    ...globalConfig,
-    connectionStrings: [
+    const expected: IAppSetting[] = [
       {
         name: 'data',
-        sensitive: true,
+        value: 'ks3l7pl3EGX1QjFFGkZTcQcdizooEKftwwd8/sZ+o4aasgytauZM4N0hz30fTVkpI821ZoMzii3yMw9H6Sz9PQ==',
         slotSetting: true,
       },
-    ],
-    appSettings: [],
-  };
+    ];
 
-  const appSettings: IAppSetting[] = [
-    {
-      name: 'data',
-      value: 'value',
-      slotSetting: true,
-    },
-  ];
+    const appSettingsMasking = new AppSettingsMasking(swapAppService, AppSettingsType.ConnectionStrings);
+    expect(appSettingsMasking.mask(appSettings, 'staging')).toStrictEqual(expected);
+  });
 
-  const expected: IAppSetting[] = [
-    {
-      name: 'data',
-      value: 'ks3l7pl3EGX1QjFFGkZTcQcdizooEKftwwd8/sZ+o4aasgytauZM4N0hz30fTVkpI821ZoMzii3yMw9H6Sz9PQ==',
-      slotSetting: true,
-    },
-  ];
+  test('AppSettingsMasking.mask() (connectionStrings) should return always hash value in connectionStrings if sensitive is false', () => {
+    const swapAppService: ISwapAppService = {
+      ...globalConfig,
+      defaultSensitive: DefaultSensitiveEnum.required,
 
-  const appSettingsMasking = new AppSettingsMasking(swapAppService, AppSettingsType.ConnectionStrings);
-  expect(appSettingsMasking.mask(appSettings, 'staging')).toStrictEqual(expected);
-});
+      connectionStrings: [
+        {
+          name: 'data',
+          sensitive: false,
+          slotSetting: true,
+        },
+      ],
+      appSettings: [],
+    };
 
-test('AppSettingsMasking.mask() (connectionStrings) should return always hash value in connectionStrings if sensitive is false', () => {
-  const swapAppService: ISwapAppService = {
-    ...globalConfig,
-    connectionStrings: [
+    const appSettings: IAppSetting[] = [
       {
         name: 'data',
-        sensitive: false,
+        value: 'value',
         slotSetting: true,
       },
-    ],
-    appSettings: [],
-  };
+    ];
 
-  const appSettings: IAppSetting[] = [
-    {
-      name: 'data',
-      value: 'value',
-      slotSetting: true,
-    },
-  ];
+    const expected: IAppSetting[] = [
+      {
+        name: 'data',
+        value: 'ks3l7pl3EGX1QjFFGkZTcQcdizooEKftwwd8/sZ+o4aasgytauZM4N0hz30fTVkpI821ZoMzii3yMw9H6Sz9PQ==',
+        slotSetting: true,
+      },
+    ];
 
-  const expected: IAppSetting[] = [
-    {
-      name: 'data',
-      value: 'ks3l7pl3EGX1QjFFGkZTcQcdizooEKftwwd8/sZ+o4aasgytauZM4N0hz30fTVkpI821ZoMzii3yMw9H6Sz9PQ==',
-      slotSetting: true,
-    },
-  ];
+    const appSettingsMasking = new AppSettingsMasking(swapAppService, AppSettingsType.ConnectionStrings);
+    expect(appSettingsMasking.mask(appSettings, 'staging')).toStrictEqual(expected);
+  });
 
-  const appSettingsMasking = new AppSettingsMasking(swapAppService, AppSettingsType.ConnectionStrings);
-  expect(appSettingsMasking.mask(appSettings, 'staging')).toStrictEqual(expected);
+  test('AppSettingsMasking.mask() (connectionStrings) should return always hash value in connectionStrings if defaultSensitive is true, and no specific config for sensitive', () => {
+    const swapAppService: ISwapAppService = {
+      ...globalConfig,
+      defaultSensitive: DefaultSensitiveEnum.true,
+
+      connectionStrings: [],
+      appSettings: [],
+    };
+
+    const appSettings: IAppSetting[] = [
+      {
+        name: 'data',
+        value: 'value',
+        slotSetting: true,
+      },
+    ];
+
+    const expected: IAppSetting[] = [
+      {
+        name: 'data',
+        value: 'ks3l7pl3EGX1QjFFGkZTcQcdizooEKftwwd8/sZ+o4aasgytauZM4N0hz30fTVkpI821ZoMzii3yMw9H6Sz9PQ==',
+        slotSetting: true,
+      },
+    ];
+
+    const appSettingsMasking = new AppSettingsMasking(swapAppService, AppSettingsType.ConnectionStrings);
+    expect(appSettingsMasking.mask(appSettings, 'staging')).toStrictEqual(expected);
+  });
 });
